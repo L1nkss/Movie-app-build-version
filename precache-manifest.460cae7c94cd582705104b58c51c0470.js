c675a8258f7f44a649dd210feec2024e8e6c151e
Сергей Харлов <l1nks@inbox.ru>
2020-09-13 22:52:52 +0500

    Updates

---
+++ precache-manifest.460cae7c94cd582705104b58c51c0470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2af758327a0042aa3c0506aa24e71af",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "7cfbc15f185cd1e0014a",
+    "url": "/movie-app/static/css/main.2030c0d3.chunk.css"
+  },
+  {
+    "revision": "d3bc773df55eaef42c07",
+    "url": "/movie-app/static/js/2.f14c483f.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/movie-app/static/js/2.f14c483f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfbc15f185cd1e0014a",
+    "url": "/movie-app/static/js/main.68728cdf.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
